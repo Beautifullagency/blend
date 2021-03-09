e96dd1d8aad3177fd106c9d57ff0d4db52e6307d
Beautifullagency <Javiercossio@beautifullagency.com>
2021-03-09 17:56:08 -0300

    agregado contenido al colapse

--- js/index.js
+++ js/index.js
@@ -3,21 +3,25 @@
 const btnSwitch = document.querySelector("#nav-switch");
 
 btnSwitch.addEventListener("click", () => {
-    document.body.classList.toggle('dark')
-    btnSwitch.classList.toggle('active')
-
-
+  document.body.classList.toggle("dark");
+  btnSwitch.classList.toggle("active");
 });
-
 
 /*Acordeon */
 const btnToggle = document.querySelector("#toggle-colapse");
 const leftColapse = document.querySelector("#colapse-side-left");
-const iconColapse = document.querySelector('.colapse-icon');
+const iconColapse = document.querySelector(".colapse-icon");
+const sideLeft = document.querySelector(".side-left");
+
 btnToggle.addEventListener("click", () => {
-   
-leftColapse.classList.toggle('colapse-side-left-active') 
-iconColapse.classList.toggle('colapse-icon-active')
-
-
+ 
+  if (sideLeft.classList.contains("side-left-active")) {
+    sideLeft.classList.remove("side-left-active");
+  } else {
+    setTimeout(function () {
+        sideLeft.classList.add("side-left-active");
+      }, 700);
+  }
+  leftColapse.classList.toggle("colapse-side-left-active");
+  iconColapse.classList.toggle("colapse-icon-active");
 });
