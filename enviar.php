f5df1a6cbb6ff51232ae97c1dd294fa8d238a843
Beautifullagency <Javiercossio@beautifullagency.com>
2021-04-19 14:17:16 -0300

    fix

--- enviar.php
+++ enviar.php
@@ -32,7 +32,7 @@ try {
     $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'info@blendaec.com';                     //SMTP username
-    $mail->Password   = 'AECServices-2020';                               //SMTP password
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
